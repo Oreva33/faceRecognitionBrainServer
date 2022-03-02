b219de81c41a7804c134d0bb8265b2aac82f60a9
Orevaoghene Ukah <oreva.ukah@yahoo.com>
2022-03-02 20:32:20 +0100

    fix server

--- server.js
+++ server.js
@@ -1,61 +1,59 @@
-const express = require('express');
-const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
-const signin = require('./control/signin');
-const register = require('./control/register');
-const id = require('./control/id');
-const image = require('./control/image');
-const Clarifai = require('clarifai')
+const express = require("express");
+const bodyParser = require("body-parser"); // latest version of exressJS now comes with Body-Parser!
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+const signin = require("./control/signin");
+const register = require("./control/register");
+const id = require("./control/id");
+const image = require("./control/image");
+const Clarifai = require("clarifai");
 
-const apps =  new Clarifai.App({
-  apiKey: 'ee1f3a82979c413fa9fc7551e27a31b8'
- });
-
-
-
+const apps = new Clarifai.App({
+  apiKey: "ee1f3a82979c413fa9fc7551e27a31b8",
+});
 
 const db = knex({
   // Enter your own database information here based on what you created
-  client: 'pg',
+  client: "pg",
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '123',
-    database : 'smart-brain'
-  }
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  },
 });
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
-
-app.get('/', (req, res)=> {
+app.get("/", (req, res) => {
   res.send("IT IS WORKING");
-})
+});
 
-app.post('/signin', (req,res) => {signin.signin(req,res,db,bcrypt)})
+app.post("/signin", (req, res) => {
+  signin.signin(req, res, db, bcrypt);
+});
 
-app.post('/register', (req,res) => {register.register(req,res,db,bcrypt)})
+app.post("/register", (req, res) => {
+  register.register(req, res, db, bcrypt);
+});
 
-app.get('/profile/:id', (req,res)=>{id.id(req,res,db,bcrypt)})
+app.get("/profile/:id", (req, res) => {
+  id.id(req, res, db, bcrypt);
+});
 
-app.put('/image', (req,res)=> {image.image(req,res,db,bcrypt)})
+app.put("/image", (req, res) => {
+  image.image(req, res, db, bcrypt);
+});
 
-app.post('/imageurl', (req,res) => {
-  const {input} = req.body
- apps.models
- .predict(
-   Clarifai.FACE_DETECT_MODEL,
-   input).then((data) => {
-     res.json(data)
-   })
-})
+app.post("/imageurl", (req, res) => {
+  const { input } = req.body;
+  apps.models.predict(Clarifai.FACE_DETECT_MODEL, input).then((data) => {
+    res.json(data);
+  });
+});
 
-
-app.listen(process.env.PORT || 3000, ()=> {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`app is running on port ${process.env.PORT}`);
-})
+});
